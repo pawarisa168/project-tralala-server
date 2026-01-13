70616addf07e3f96ef15ce724c25aabd4aaddf7c
Pawarisa <pawarisa.m20@gmail.com>
2026-01-13 13:30:06 +0700

    initialize backend structure

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
