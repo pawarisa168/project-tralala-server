0a2d24069a6825a6fda4f6443a3924eb692a0ec8
unknown <wrpxdouble1135@gmail.com>
2026-01-13 15:00:36 +0700

    added routes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import { app } from "./app.js";
+import { connectDB } from "./config/db.js";
 
 const port = process.env.PORT || 3000;
 
 try {
+  await connectDB();
   app.listen(port, () => {
     console.log(`server runing on port: ${port}🟩`);
   });
