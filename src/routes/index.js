331a2632806fe583fed010d759ef38687f8a875b
unknown <wrpxdouble1135@gmail.com>
2026-01-14 12:18:57 +0700

    setting routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,9 @@ import { Router } from "express";
 
 export const router = Router();
 
+router.get("/", home);
+router.get("about", about);
+router.get("serviceslist", serviceslist);
+router.get("productlist", productlist);
+router.get("contact", contact);
 router.get("/login", usersLogin);
--- src/routes/login/index.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { router as usersRouters } from "./users.routes.js";
-
-export const router = Router();
-
-router.use("/users", usersRouters);
