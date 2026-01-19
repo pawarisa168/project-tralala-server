430cc494fbf42df7aac91d059685edfee32abcbb
unknown <wrpxdouble1135@gmail.com>
2026-01-19 14:24:20 +0700

    confilt routes

--- src/routes/v1/services.routes.js
+++ src/routes/v1/services.routes.js
@@ -3,7 +3,10 @@ import { getServices, getServicesId } from "../../controller/services.controller
 
 const serviceRouter = Router();
 
+// ดูบริการทั้งหมด
 serviceRouter.get("/serviceslist", getServices);
+
+// ดูข้อมูลบริการตามไอดี
 serviceRouter.get("/serviceslist/:id", getServicesId);
 
 export default serviceRouter;
