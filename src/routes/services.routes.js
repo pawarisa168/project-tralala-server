dbcfeadb0a774299c16bccc47388a1a75d75e6c5
Pawarisa <76102211+pawarisa168@users.noreply.github.com>
2026-01-19 10:42:42 +0700

    Delete src/routes/services.routes.js
    fix: move file to version 1.

--- src/routes/services.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getServices, getServicesId } from "../controller/services.controller.js";
-
-const serviceRouter = Router();
-
-serviceRouter.get("/services", getServices);
-serviceRouter.get("/services/:id", getServicesId);
-
-export default serviceRouter;
