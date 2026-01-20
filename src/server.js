b9ddec860dd83f81d407923653a3344fd0dfe276
Pawarisa <pawarisa.m20@gmail.com>
2026-01-20 14:53:34 +0700

    add cors for dev

--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@ const corsOptions = {
     "http://localhost:5174",
     "http://localhost:5175",
     "https://project-tralala-react.vercel.app",
+    "https://project-tralala-react-dev.vercel.app",
   ],
   credentials: true, // ✅ allow cookies to be sent
 };
