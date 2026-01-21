36d8947cac24ad46da850f8ca1f4573aab79b5bb
meanwrs <meen.warissara@hotmail.com>
2026-01-21 11:29:55 +0700

    added routes caregiver logs and test api is done

--- src/routes/v1/log.routes.js
+++ src/routes/v1/log.routes.js
@@ -1,3 +1,8 @@
 import { Router } from "express";
+import {getCaregiverLogs} from "../../modules/caregiver/caregiver.controller.js"
 
 export const router = Router();
+
+// get data caregiver log http://localhost:3000/api/v1/caregiver-logs
+router.get("/", getCaregiverLogs);
+
